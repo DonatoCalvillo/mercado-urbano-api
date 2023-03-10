e03dbbda17b28c2f39e6f99b6846fa1f25695800
Donato Calvillo <donato.calvillo@3mium.com>
2023-03-10 01:03:29 -0600

    release: update README

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { EventModule } from './event/event.module';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
-      username: "root",
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       entities:[],
       autoLoadEntities: true,

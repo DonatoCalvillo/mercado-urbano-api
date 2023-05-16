b684c1cf60bf687191902f767471c3cd66a5ca46
Donato Calvillo <donato.calvillo@3mium.com>
2023-05-16 15:58:00 -0600

    refactor: remove space

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,6 @@ import { EventModule } from './event/event.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-
       entities: [],
       autoLoadEntities: true,
       synchronize: false, //In production most be FALSE

db819025cf5fd557c96f7f3d63a626305441602a
Donato Calvillo <donato.calvillo@3mium.com>
2023-05-16 00:07:07 -0600

    fix: fix pagination dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,20 @@
 import { Controller, Param, Query, Delete, Get } from '@nestjs/common';
 import { Auth, ValidRoles } from 'src/auth/decorators';
-import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-
   constructor(private readonly userService: UserService) {}
 
   @Get('getAll')
-  @Auth( ValidRoles.admin, ValidRoles.superAdmin )
-  getAllUsers(@Query() paginationDto:PaginationDto) {
-    return this.userService.getAllUsers(paginationDto)
+  @Auth(ValidRoles.admin, ValidRoles.superAdmin)
+  getAllUsers() {
+    return this.userService.getAllUsers();
   }
 
   @Delete('deleteUser')
-  @Auth( ValidRoles.admin, ValidRoles.superAdmin )
-  deleteUser(@Query('id') id: string){
+  @Auth(ValidRoles.admin, ValidRoles.superAdmin)
+  deleteUser(@Query('id') id: string) {
     return this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,52 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Usuario } from '../auth/entities';
 import { Repository } from 'typeorm';
-import { PaginationDto } from '../common/dtos/pagination.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(Usuario)
-    private readonly userRepository: Repository<Usuario>
-  ){}
+    private readonly userRepository: Repository<Usuario>,
+  ) {}
 
-  async getAllUsers( paginationDto: PaginationDto ) {
-    const { limit = 10, offset = 0 } = paginationDto
-
-    const users = await this.userRepository.createQueryBuilder("usuario")
-    .select([
-      'usuario.matricula',
-      'usuario.nombre',
-      'usuario.apellido_paterno',
-      'usuario.apellido_materno',
-      'usuario.puntos',
-      'usuario.correo',
-      'usuario.telefono'
-    ])  
-    .innerJoin("usuario.rol", "rol")
-    .where("rol.nombre =:nombre", {nombre: "Usuario"})
-    .andWhere("usuario.activo = 1")
-    .orderBy("usuario.puntos", "DESC")
-    .getMany()
-
-    return users
-  }
-
-  async deleteUser ( matricula: string ){
-    try{  
-      const user = await this.userRepository.findOne({ where: { matricula } })
-
-      if(!user)
-        return{
-          status: "FAIL",
-          message: "No se encontro al usuario."
-        }
-      
-      user.activo = false
-
-      await this.userRepository.save(user)
-      
-      const users = await this.userRepository.createQueryBuilder("usuario")
+  async getAllUsers() {
+    const users = await this.userRepository
+      .createQueryBuilder('usuario')
       .select([
         'usuario.matricula',
         'usuario.nombre',
@@ -55,22 +20,53 @@ export class UserService {
         'usuario.apellido_materno',
         'usuario.puntos',
         'usuario.correo',
-        'usuario.telefono'
-      ])  
-      .innerJoin("usuario.rol", "rol")
-      .where("rol.nombre =:nombre", {nombre: "Usuario"})
-      .andWhere("usuario.activo = 1")
-      .orderBy("usuario.puntos", "DESC")
-      .getMany()
+        'usuario.telefono',
+      ])
+      .innerJoin('usuario.rol', 'rol')
+      .where('rol.nombre =:nombre', { nombre: 'Usuario' })
+      .andWhere('usuario.activo = 1')
+      .orderBy('usuario.puntos', 'DESC')
+      .getMany();
 
-      return{
-        status: "OK",
-        message: "Usuario eliminado correctamente.",
-        data: users
+    return users;
+  }
 
-      }
-    }catch(error){
+  async deleteUser(matricula: string) {
+    try {
+      const user = await this.userRepository.findOne({ where: { matricula } });
 
-    }
+      if (!user)
+        return {
+          status: 'FAIL',
+          message: 'No se encontro al usuario.',
+        };
+
+      user.activo = false;
+
+      await this.userRepository.save(user);
+
+      const users = await this.userRepository
+        .createQueryBuilder('usuario')
+        .select([
+          'usuario.matricula',
+          'usuario.nombre',
+          'usuario.apellido_paterno',
+          'usuario.apellido_materno',
+          'usuario.puntos',
+          'usuario.correo',
+          'usuario.telefono',
+        ])
+        .innerJoin('usuario.rol', 'rol')
+        .where('rol.nombre =:nombre', { nombre: 'Usuario' })
+        .andWhere('usuario.activo = 1')
+        .orderBy('usuario.puntos', 'DESC')
+        .getMany();
+
+      return {
+        status: 'OK',
+        message: 'Usuario eliminado correctamente.',
+        data: users,
+      };
+    } catch (error) {}
   }
 }

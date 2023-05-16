6e9f9cf2c11324949778782470fe0b01b64b60a3
Donato Calvillo <donato.calvillo@3mium.com>
2023-05-16 00:56:27 -0600

    fix: cors error 6

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
   });
 
   app.setGlobalPrefix('api');
